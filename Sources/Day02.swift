bae1597aaf39afb2733e2312d1cf7e93d851b202
Jayson Rhynas <jayrhynas@gmail.com>
2024-12-17 10:00:12 -0500

    Day 2 part 1

--- Sources/Day02.swift
+++ Sources/Day02.swift
@@ -4,8 +4,39 @@ import Parsing
 struct Day02: AdventDay {
   var data: String
 
-  func part1() -> Int {
-    return 0
+  struct LevelsParser: Parser {
+    var body: some Parser<Substring, [[Int]]> {
+      Many {
+        Many(1...) {
+          Int.parser()
+        } separator: {
+          Whitespace(.horizontal)
+        }
+      } separator: {
+        "\n"
+      } terminator: {
+        "\n"
+      }
+    }
+  }
+
+  func isSafe(_ report: [Int]) -> Bool {
+    var sign: Int?
+    for (prev, next) in zip(report, report.dropFirst()) {
+      let diff = next - prev
+      if abs(diff) > 3 { return false }
+      if sign == nil {
+        sign = diff.signum()
+      } else if sign != diff.signum() {
+        return false
+      }
+    }
+    return true
+  }
+  
+  func part1() throws -> Int {
+    try LevelsParser().parse(data)
+      .count(where: isSafe(_:))
   }
 
   func part2() -> Int {
--- Tests/Day02.swift
+++ Tests/Day02.swift
@@ -3,11 +3,19 @@ import Testing
 @testable import AdventOfCode
 
 struct Day02Tests {
-
+  let data = """
+  7 6 4 2 1
+  1 2 7 8 9
+  9 7 6 2 1
+  1 3 2 4 5
+  8 6 4 4 1
+  1 3 6 7 9
+  
+  """
 
 
   @Test func testPart1() async throws {
-    #expect(true)
+    try #expect(Day02(data: data).part1() == 2)
   }
 
   @Test func testPart2() async throws {
