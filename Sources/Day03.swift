65161e4cc86103adcf0e1a10ee7744235e97640e
Jayson Rhynas <jayrhynas@gmail.com>
2024-12-17 10:37:54 -0500

    Day 3 part 2

--- Sources/Day03.swift
+++ Sources/Day03.swift
@@ -14,7 +14,7 @@ struct Day03: AdventDay {
     }
   }
   
-  struct InstructionParser: Parser {
+  struct Part1InstructionParser: Parser {
     var body: some Parser<Substring, [(Int, Int)]> {
       Many(into: []) { (pairs: inout [(Int, Int)], new: (Int, Int)?) in
         if let value = new {
@@ -30,11 +30,45 @@ struct Day03: AdventDay {
   }
   
   func part1() throws -> Int {
-    try InstructionParser().parse(data)
+    try Part1InstructionParser().parse(data)
       .map { $0 * $1 }.reduce(0, +)
   }
 
-  func part2() -> Int {
-    return 0
+  enum Instruction {
+    case enable, disable, mul(Int, Int)
+  }
+  
+  struct Part2InstructionParser: Parser {
+    var body: some Parser<Substring, [Instruction]> {
+      Many(into: []) { (pairs: inout [Instruction], new: Instruction?) in
+        if let value = new {
+          pairs.append(value)
+        }
+      } element: {
+        OneOf(output: Instruction?.self) {
+          MulParser().map { Optional(.mul($0.0, $0.1)) }
+          "do()".map { Optional(.enable) }
+          "don't()".map { Optional(.disable) }
+          First().map { _ in nil }
+        }
+      }
+    }
+  }
+  
+  func part2() throws -> Int {
+    let instructions = try Part2InstructionParser().parse(data)
+    
+    var enabled = true
+    
+    var sum = 0
+    for instruction in instructions {
+      switch instruction {
+      case .enable: enabled = true
+      case .disable: enabled = false
+      case let .mul(x, y): sum += (enabled ? x * y : 0)
+      }
+    }
+    
+    return sum
   }
 }
--- Tests/Day03.swift
+++ Tests/Day03.swift
@@ -3,16 +3,20 @@ import Testing
 @testable import AdventOfCode
 
 struct Day03Tests {
-  let data = """
+  let part1data = """
   xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
   """
 
 
   @Test func testPart1() async throws {
-    try #expect(Day03(data: data).part1() == 161)
+    try #expect(Day03(data: part1data).part1() == 161)
   }
 
+  let part2data = """
+  xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+  """
+  
   @Test func testPart2() async throws {
-    #expect(true)
+    try #expect(Day03(data: part2data).part2() == 48)
   }
 }
